bd0fd8eaf6e78d7aad9659cac9eb5ccb1e9da231
Nicole Uyigue-Sivek <nicoleusivek@gmail.com>
2020-09-05 23:56:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,9 @@ end
 
 def find_valid_calls(planeteer_calls)
   	valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
- 	planeteer_calls.find |element| 
+ 	planeteer_calls.find do |element| 
 		valid_calls.include?(element)
 	end
 end
+
+
